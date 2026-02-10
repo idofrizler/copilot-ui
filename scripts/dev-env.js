6c9ab111728fc493b6f2a7f0334737578f16e821
maxtara <max.tarasuik@gmail.com>
2026-02-11 10:50:36 +1100

    chore: set all three env vars in dev script

--- scripts/dev-env.js
+++ scripts/dev-env.js
@@ -3,6 +3,7 @@ const { join } = require('path');
 const { execSync } = require('child_process');
 
 const devHome = join(homedir(), '.cooper-dev');
+process.env.XDG_CONFIG_HOME = devHome;
 process.env.XDG_STATE_HOME = devHome;
 process.env.COPILOT_SESSIONS_HOME = join(devHome, 'sessions');
 execSync('electron-vite dev', { stdio: 'inherit', env: process.env });
